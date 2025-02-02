a8a76ba498652153d69b5e03539b2449c70dac68
Mert Olkun <mert_olkun_26@outlook.com>
2025-02-02 20:52:25 +0300

    02.02.2025

---
+++ SelectFile_class.cs
@@ -0,0 +1,45 @@
+public class FilePathSelector
+{
+    public string selectFile()
+    {
+        Console.WriteLine(
+            $"\nif you want to select file, add '-' in front of the path.\n example: -{Directory.GetCurrentDirectory()}\nif you want to search file, write a path.\n example: /home/name/folder\n"
+        );
+        while (true)
+        {
+            string dir = Console.ReadLine() ?? "Null";
+            if (dir.StartsWith('-') == false)
+            {
+                string[] Folders = Directory.GetDirectories(dir);
+                string[] Files = Directory.GetFiles(dir);
+                Console.WriteLine("\n");
+                foreach (var folder in Folders)
+                {
+                    Console.WriteLine("Folder: " + folder);
+                }
+                foreach (var file in Files)
+                {
+                    Console.WriteLine("File: " + file);
+                }
+            }
+            else if (dir.StartsWith('-') == true)
+            {
+                string[] x = dir.Split('-');
+                string pathString = x[1];
+                return pathString;
+            }
+        }
+    }
+}
+
+public class Entries
+{
+    public string? Name { get; set; }
+    public string? Id { get; set; }
+}
+
+public class Name
+{
+    public List<Entries>? Entries { get; set; }
+    public string? Cursor { get; set; }
+}
